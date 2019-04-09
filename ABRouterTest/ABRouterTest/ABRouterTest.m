837a2fccc2e0f4db93840ffaf303bb884ab6e79a
HarrisonXi <gpra8764@gmail.com>
2019-04-09 15:16:17 +0800

    add unit test for abtest feature

--- ABRouterTest/ABRouterTest/ABRouterTest.m
+++ ABRouterTest/ABRouterTest/ABRouterTest.m
@@ -181,6 +181,151 @@
     XCTAssertEqualObjects(vc.params[@"f"], @"5");
 }
 
+- (void)testAB1
+{
+    [_router map:@"/a/:aId/" toControllerClass:[UITableViewController class] abOption:ABRouterOptionNone];
+    [_router map:@"/a/:aId/" toControllerClass:[UITabBarController class] abOption:ABRouterOptionA];
+    [_router map:@"/a/:aId/" toControllerClass:[UINavigationController class] abOption:ABRouterOptionB];
+    [_router map:@"/a/:aId/" toControllerClass:[UISplitViewController class] abOption:ABRouterOptionC];
+    [_router map:@"/a/:aId/" toControllerClass:[UICollectionViewController class] abOption:ABRouterOptionD];
+    
+    XCTAssertTrue([_router canMapController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionNone]);
+    XCTAssertTrue([_router canMapController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionA]);
+    XCTAssertTrue([_router canMapController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionB]);
+    XCTAssertTrue([_router canMapController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionC]);
+    XCTAssertTrue([_router canMapController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionD]);
+    
+    UIViewController *vc = [_router matchController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionNone];
+    XCTAssertEqualObjects([vc class], [UITableViewController class]);
+    vc = [_router matchController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionA];
+    XCTAssertEqualObjects([vc class], [UITabBarController class]);
+    vc = [_router matchController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionB];
+    XCTAssertEqualObjects([vc class], [UINavigationController class]);
+    vc = [_router matchController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionC];
+    XCTAssertEqualObjects([vc class], [UISplitViewController class]);
+    vc = [_router matchController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionD];
+    XCTAssertEqualObjects([vc class], [UICollectionViewController class]);
+    vc = [_router matchController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionAll];
+    XCTAssertEqualObjects([vc class], [UITabBarController class]);
+    vc = [_router matchController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionA | ABRouterOptionB];
+    XCTAssertEqualObjects([vc class], [UITabBarController class]);
+    vc = [_router matchController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionC | ABRouterOptionB];
+    XCTAssertEqualObjects([vc class], [UINavigationController class]);
+    vc = [_router matchController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionC | ABRouterOptionD];
+    XCTAssertEqualObjects([vc class], [UISplitViewController class]);
+    
+    // test remove route
+    [_router map:@"/a/:aId/" toControllerClass:NULL abOption:ABRouterOptionA];
+    
+    XCTAssertFalse([_router canMapController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionA]);
+    
+    vc = [_router matchController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionAll];
+    XCTAssertEqualObjects([vc class], [UINavigationController class]);
+    vc = [_router matchController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionA | ABRouterOptionB];
+    XCTAssertEqualObjects([vc class], [UINavigationController class]);
+    
+    // setting controller block will not reset controller class route
+    [_router map:@"/a/:aId/" toControllerBlock:^UIViewController *(NSDictionary *params) {
+        return [UITabBarController new];
+    } abOption:ABRouterOptionA];
+
+    XCTAssertTrue([_router canMapController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionA]);
+    
+    vc = [_router matchController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionAll];
+    XCTAssertEqualObjects([vc class], [UITabBarController class]);
+    vc = [_router matchController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionA | ABRouterOptionB];
+    XCTAssertEqualObjects([vc class], [UITabBarController class]);
+    
+    // setting action block will reset controller route
+    [_router map:@"/a/:aId/" toActionBlock:^id(NSDictionary *params) {
+        return nil;
+    } abOption:ABRouterOptionB];
+    
+    XCTAssertFalse([_router canMapController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionNone]);
+    XCTAssertFalse([_router canMapController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionA]);
+    XCTAssertFalse([_router canMapController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionB]);
+    XCTAssertFalse([_router canMapController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionC]);
+    XCTAssertFalse([_router canMapController:@"/a/1/?b=4&c=5" abOption:ABRouterOptionD]);
+    XCTAssertFalse([_router canMapAction:@"/a/1/?b=4&c=5" abOption:ABRouterOptionNone]);
+    XCTAssertFalse([_router canMapAction:@"/a/1/?b=4&c=5" abOption:ABRouterOptionA]);
+    XCTAssertTrue([_router canMapAction:@"/a/1/?b=4&c=5" abOption:ABRouterOptionB]);
+    XCTAssertFalse([_router canMapAction:@"/a/1/?b=4&c=5" abOption:ABRouterOptionC]);
+    XCTAssertFalse([_router canMapAction:@"/a/1/?b=4&c=5" abOption:ABRouterOptionD]);
+}
+
+- (void)testAB2
+{
+    [_router map:@"/b/:bId/" toActionBlock:^id(NSDictionary *params) {
+        return @(0);
+    } abOption:ABRouterOptionNone];
+    [_router map:@"/b/:bId/" toActionBlock:^id(NSDictionary *params) {
+        return @(1);
+    } abOption:ABRouterOptionA];
+    [_router map:@"/b/:bId/" toActionBlock:^id(NSDictionary *params) {
+        return @(2);
+    } abOption:ABRouterOptionB];
+    [_router map:@"/b/:bId/" toActionBlock:^id(NSDictionary *params) {
+        return @(3);
+    } abOption:ABRouterOptionC];
+    [_router map:@"/b/:bId/" toActionBlock:^id(NSDictionary *params) {
+        return @(4);
+    } abOption:ABRouterOptionD];
+    
+    XCTAssertTrue([_router canMapAction:@"/b/1/?d=4&e=5" abOption:ABRouterOptionNone]);
+    XCTAssertTrue([_router canMapAction:@"/b/1/?d=4&e=5" abOption:ABRouterOptionA]);
+    XCTAssertTrue([_router canMapAction:@"/b/1/?d=4&e=5" abOption:ABRouterOptionB]);
+    XCTAssertTrue([_router canMapAction:@"/b/1/?d=4&e=5" abOption:ABRouterOptionC]);
+    XCTAssertTrue([_router canMapAction:@"/b/1/?d=4&e=5" abOption:ABRouterOptionD]);
+    
+    ABRouterActionBlock block = [_router matchActionBlock:@"/b/1/?d=4&e=5" abOption:ABRouterOptionNone];
+    NSNumber *value = block(nil);
+    XCTAssertEqualObjects(value, @(0));
+    block = [_router matchActionBlock:@"/b/1/?d=4&e=5" abOption:ABRouterOptionA];
+    value = block(nil);
+    XCTAssertEqualObjects(value, @(1));
+    block = [_router matchActionBlock:@"/b/1/?d=4&e=5" abOption:ABRouterOptionB];
+    value = block(nil);
+    XCTAssertEqualObjects(value, @(2));
+    block = [_router matchActionBlock:@"/b/1/?d=4&e=5" abOption:ABRouterOptionC];
+    value = block(nil);
+    XCTAssertEqualObjects(value, @(3));
+    block = [_router matchActionBlock:@"/b/1/?d=4&e=5" abOption:ABRouterOptionD];
+    value = block(nil);
+    XCTAssertEqualObjects(value, @(4));
+    value = [_router callActionBlock:@"/b/1/?d=4&e=5" abOption:ABRouterOptionAll];
+    XCTAssertEqualObjects(value, @(1));
+    value = [_router callActionBlock:@"/b/1/?d=4&e=5" abOption:ABRouterOptionA | ABRouterOptionB];
+    XCTAssertEqualObjects(value, @(1));
+    value = [_router callActionBlock:@"/b/1/?d=4&e=5" abOption:ABRouterOptionC | ABRouterOptionB];
+    XCTAssertEqualObjects(value, @(2));
+    value = [_router callActionBlock:@"/b/1/?d=4&e=5" abOption:ABRouterOptionC | ABRouterOptionD];
+    XCTAssertEqualObjects(value, @(3));
+    
+    // test remove route
+    [_router map:@"/b/:bId/" toActionBlock:nil abOption:ABRouterOptionA];
+    
+    XCTAssertFalse([_router canMapAction:@"/b/1/?d=4&e=5" abOption:ABRouterOptionA]);
+    
+    value = [_router callActionBlock:@"/b/1/?d=4&e=5" abOption:ABRouterOptionAll];
+    XCTAssertEqualObjects(value, @(2));
+    value = [_router callActionBlock:@"/b/1/?d=4&e=5" abOption:ABRouterOptionA | ABRouterOptionB];
+    XCTAssertEqualObjects(value, @(2));
+
+    // setting controller class or block will reset action route
+    [_router map:@"/b/:bId/" toControllerClass:[UITableViewController class] abOption:ABRouterOptionB];
+    
+    XCTAssertFalse([_router canMapController:@"/b/1/?d=4&e=5" abOption:ABRouterOptionNone]);
+    XCTAssertFalse([_router canMapController:@"/b/1/?d=4&e=5" abOption:ABRouterOptionA]);
+    XCTAssertTrue([_router canMapController:@"/b/1/?d=4&e=5" abOption:ABRouterOptionB]);
+    XCTAssertFalse([_router canMapController:@"/b/1/?d=4&e=5" abOption:ABRouterOptionC]);
+    XCTAssertFalse([_router canMapController:@"/b/1/?d=4&e=5" abOption:ABRouterOptionD]);
+    XCTAssertFalse([_router canMapAction:@"/b/1/?d=4&e=5" abOption:ABRouterOptionNone]);
+    XCTAssertFalse([_router canMapAction:@"/b/1/?d=4&e=5" abOption:ABRouterOptionA]);
+    XCTAssertFalse([_router canMapAction:@"/b/1/?d=4&e=5" abOption:ABRouterOptionB]);
+    XCTAssertFalse([_router canMapAction:@"/b/1/?d=4&e=5" abOption:ABRouterOptionC]);
+    XCTAssertFalse([_router canMapAction:@"/b/1/?d=4&e=5" abOption:ABRouterOptionD]);
+}
+
 - (void)testAppUrlScheme
 {
     [_router map:@"/a/:aId/" toControllerClass:[UITableViewController class]];
